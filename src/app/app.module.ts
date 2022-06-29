2f5075c8796b0cf2a9f57c96ae190c4aa3d8b558
kasimibrahim <kasim.ibrahim@ashesi.edu.gh>
2022-06-29 17:46:51 +0000

    Layout of ppokeman project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    MainScreenComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
